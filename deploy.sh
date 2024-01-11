e098ceeb30b1fe49ac2161bb8310df5e844e5d45
Филиппенко Сергей Викторович <SFilippenko@alfabank.ru>
2024-01-11 13:42:53 +0300

    auto deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-echo("Installing modules")
+echo "Installing modules"
 npm i
 
-echo("Building")
+echo "Building"
 npm run build
 
 cp ./nginx.conf /etc/ngix/sites-enabled/default
 sudo service nginx restart
-echo("Done")
+echo "Done"
